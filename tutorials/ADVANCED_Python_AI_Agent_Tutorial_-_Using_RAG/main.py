d0426a07adc17d8ddb501468bc8f4957f5f92af4
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2026-04-19 17:47:35 +0200

    Add main functionality and data files for Advanced Python AI Agent Tutorial: Implement main.py for querying population data using Pandas, create prompts.py for query instructions, and add population dataset and Canada information PDF for reference.

---
+++ tutorials/ADVANCED_Python_AI_Agent_Tutorial_-_Using_RAG/main.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+from dotenv import load_dotenv
+from llama_index.experimental.query_engine.pandas import PandasQueryEngine
+from prompts import instruction_str, new_prompt
+
+env_path = Path(__file__).with_name("openai.env")
+load_dotenv(dotenv_path=env_path)
+
+population_path = os.path.join("data", "population.csv")
+population_df = pd.read_csv(population_path)
+
+population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
+
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+
+population_query_engine.query("What is the population of Canada?")
