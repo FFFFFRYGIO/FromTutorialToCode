135a269a81a1b1b42ac550a0aeab3fc6724bbc27
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2026-05-23 20:12:38 +0200

    (IDE problems resolution) Refactor imports and update environment variable access in main.py: Rearranged import statements for better organization and changed the method of accessing the FIRECRAWL_API_KEY to use os.environ for improved reliability.

--- tutorials/Python_Advanced_AI_Agent_Tutorial_-_LangGraph,_LangChain,_Firecrawl_&_More!/simple-agent/main.py
+++ tutorials/Python_Advanced_AI_Agent_Tutorial_-_LangGraph,_LangChain,_Firecrawl_&_More!/simple-agent/main.py
@@ -1,11 +1,12 @@
-from mcp import ClientSession, StdioServerParameters
-from mcp.client.stdio import stdio_client
-from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
 import asyncio
 import os
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv("openai.env")
 load_dotenv("firecrawl.env")
@@ -13,15 +14,14 @@ load_dotenv("firecrawl.env")
 model = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0,
-    openai_api_key=os.getenv("OPENAI_API_KEY")
 )
 
 server_params = StdioServerParameters(
     command="npx",
     env={
-        "FIRECRAWL_API_KEY": os.getenv("FIRECRAWL_API_KEY"),
+        "FIRECRAWL_API_KEY": os.environ["FIRECRAWL_API_KEY"],
     },
-    args=["firecrawl-mcp"]
+    args=["firecrawl-mcp"],
 )
 
 
